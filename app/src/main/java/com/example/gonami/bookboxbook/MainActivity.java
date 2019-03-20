955e60a6f5d45b7becc627a473d78b4a47c247ac
yoosunnam <ysnam19@naver.com>
2019-03-20 15:33:35 +0900

    Bottom Navigation View icon color solved

--- app/src/main/java/com/example/gonami/bookboxbook/MainActivity.java
+++ app/src/main/java/com/example/gonami/bookboxbook/MainActivity.java
@@ -1,31 +1,92 @@
 package com.example.gonami.bookboxbook;
 
-import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.FrameLayout;
+
+import static android.support.design.widget.BottomNavigationView.*;
 
 public class MainActivity extends AppCompatActivity {
 
-    BottomNavigationView bottomNavigationView;
     SearchFragment searchFragment;
     BookMarkFragment bookMarkFragment;
     AddFragment addFragment;
     TransactionListFragment transactionListFragment;
     MyPageFragment myPageFragment;
 
-    private boolean loadFragment(Fragment fragment) {
-        if (fragment != null) {
-            getSupportFragmentManager()
-                    .beginTransaction()
-                    .replace(R.id.frame_layout, fragment)
-                    .commit();
+    BottomNavigationView bottomNavigationView;
+    Fragment activeFragment;
+    FragmentManager fragmentManager;
+    FrameLayout frameLayout;
+
+    private OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener(){
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            if ((menuItem.getItemId() == R.id.navigation_search && activeFragment == searchFragment) ||
+                    (menuItem.getItemId() == R.id.navigation_bookmark && activeFragment == bookMarkFragment) ||
+                    (menuItem.getItemId() == R.id.navigation_add && activeFragment == addFragment) ||
+                    (menuItem.getItemId() == R.id.navigation_list && activeFragment == transactionListFragment) ||
+                    (menuItem.getItemId() == R.id.navigation_mypage && activeFragment == myPageFragment)) {
+                return false;
+            }
+
+            switch (menuItem.getItemId()) {
+                case R.id.navigation_search:
+                    fragmentManager.beginTransaction()
+                            .addToBackStack(null)
+                            .replace(R.id.frame_layout, searchFragment, "Search")
+                            .commit();
+                    activeFragment = searchFragment;
+                    return true;
+                case R.id.navigation_bookmark:
+                    fragmentManager.beginTransaction()
+                            .addToBackStack(null)
+                            .replace(R.id.frame_layout, bookMarkFragment, "BookMark")
+                            .commit();
+                    activeFragment = bookMarkFragment;
+                    return true;
+                case R.id.navigation_add:
+                    fragmentManager.beginTransaction()
+                            .addToBackStack(null)
+                            .replace(R.id.frame_layout, addFragment, "Add")
+                            .commit();
+                    activeFragment = addFragment;
+                    return true;
+                case R.id.navigation_list:
+                    fragmentManager.beginTransaction()
+                            .addToBackStack(null)
+                            .replace(R.id.frame_layout, transactionListFragment, "TransactionList")
+                            .commit();
+                    activeFragment = transactionListFragment;
+                    return true;
+                case R.id.navigation_mypage:
+                    fragmentManager.beginTransaction()
+                            .addToBackStack(null)
+                            .replace(R.id.frame_layout, myPageFragment, "MyPage")
+                            .commit();
+                    activeFragment = myPageFragment;
+                    return true;
+            }
+            bottomNavigationView.setSelectedItemId(menuItem.getItemId());
+            return false;
         }
-        return false;
-    }
+    };
+
+//    private boolean loadFragment(Fragment fragment) {
+//        if (fragment != null) {
+//            getSupportFragmentManager()
+//                    .beginTransaction()
+//                    .replace(R.id.frame_layout, fragment)
+//                    .commit();
+//        }
+//        return false;
+//    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,33 +100,22 @@ public class MainActivity extends AppCompatActivity {
         myPageFragment = MyPageFragment.newInstance();
 
         bottomNavigationView = findViewById(R.id.bottom_navigation);
-//        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-//            @Override
-//            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-//                Fragment fragment = null;
-//                switch (menuItem.getItemId()) {
-//                    case R.id.navigation_search:
-//                        fragment = new SearchFragment();
-//                        break;
-//                    case R.id.navigation_bookmark:
-//                        fragment = new BookMarkFragment();
-//                        break;
-//                    case R.id.navigation_add:
-//                        fragment = new AddFragment();
-//                        break;
-//                    case R.id.navigation_list:
-//                        fragment = new TransactionListFragment();
-//                        break;
-//                    case R.id.navigation_mypage:
-//                        fragment = new MyPageFragment();
-//                        break;
-//                }
-//                return loadFragment(fragment);
-//            }
-//
-//        });
-//
-//        loadFragment(new SearchFragment());
+        bottomNavigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        fragmentManager = getSupportFragmentManager();
+
+        setFragmentManager();
 
     }
+
+    private void setFragmentManager() {
+        fragmentManager.beginTransaction()
+                .addToBackStack(null)
+                .replace(R.id.frame_layout, searchFragment, "Search")
+                .commit();
+        fragmentManager.beginTransaction().setCustomAnimations(R.anim.abc_fade_in, R.anim.abc_fade_out);
+        activeFragment = searchFragment;
+    }
+
+
 }
