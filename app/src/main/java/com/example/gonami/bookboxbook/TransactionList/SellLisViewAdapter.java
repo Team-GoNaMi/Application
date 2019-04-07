7965418bd04e058a0f352771f763d719b7393a5d
yuseon27 <ysnam19@naver.com>
2019-04-08 07:14:36 +0900

    Shows Sell List

--- app/src/main/java/com/example/gonami/bookboxbook/TransactionList/SellLisViewAdapter.java
+++ app/src/main/java/com/example/gonami/bookboxbook/TransactionList/SellLisViewAdapter.java
@@ -1,4 +1,67 @@
 package com.example.gonami.bookboxbook.TransactionList;
 
-public class SellLisViewAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.gonami.bookboxbook.R;
+
+import java.util.ArrayList;
+
+public class SellLisViewAdapter extends BaseAdapter {
+
+    private ArrayList<String> bookList;
+
+    public SellLisViewAdapter(ArrayList<String> sellList) { this.bookList = sellList; }
+
+    @Override
+    public int getCount() {
+        return bookList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return bookList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        final Context parentContext = parent.getContext();
+
+        if (convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) parentContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.book_list_transaction, parent, false);
+        }
+
+        ImageView ivBookImage = convertView.findViewById(R.id.img_book);
+        TextView tvBookName = convertView.findViewById(R.id.tv_book_name);
+        TextView tvBookInfo = convertView.findViewById(R.id.tv_book_info);
+        TextView tvSchoolNames = convertView.findViewById(R.id.tv_book_schoolname);
+        final Button btnBookState = convertView.findViewById(R.id.btn_book_state);
+        TextView tvOriginPrice = convertView.findViewById(R.id.tv_original_price);
+        TextView tvBookPrice = convertView.findViewById(R.id.tv_book_price);
+
+        String bookRegisterNum = bookList.get(position);
+
+        // TODO DB에서 불러와서 해당 책 등록번호에 맞는 책 이미지, 책 이름, 책 정보, 거래정보 등 불러와서 띄우기
+        ivBookImage.setImageAlpha(R.mipmap.ic_launcher);
+        tvBookName.setText(bookRegisterNum);
+        tvBookInfo.setText("이승윤 / 플레디스");
+        tvSchoolNames.setText("중앙대 서울캠, 숭실대");
+        tvOriginPrice.setText("5000원");
+        tvBookPrice.setText("5000원");
+        btnBookState.setText("책을 가져가주세욤!");
+
+        return convertView;
+    }
 }
--- app/src/main/java/com/example/gonami/bookboxbook/TransactionList/SellListFragment.java
+++ app/src/main/java/com/example/gonami/bookboxbook/TransactionList/SellListFragment.java
@@ -7,12 +7,19 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.example.gonami.bookboxbook.R;
+
+import java.util.ArrayList;
 
 public class SellListFragment extends Fragment {
 
     private View thisView = null;
+
+    private ListView sellListView;
+    private SellLisViewAdapter sellLisViewAdapter;
+    private ArrayList<String> sellList;
 
     public SellListFragment() {
 
@@ -36,6 +43,17 @@ public class SellListFragment extends Fragment {
 
         if (thisView == null)
             thisView = inflater.inflate(R.layout.fragment_list_sell, null);
+
+        sellList = new ArrayList<String>();
+
+        // Test
+        sellList.add("여기는!!!!");
+        sellList.add("내가 판 책");
+        sellList.add("입 니 당 ! ! !");
+
+        sellListView = thisView.findViewById(R.id.lv_sell_list);
+        sellLisViewAdapter = new SellLisViewAdapter(sellList);
+        sellListView.setAdapter(sellLisViewAdapter);
 
         return thisView;
     }
