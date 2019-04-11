7c9a3e1e9e0051c859b8693024dd86fa4674199f
KoHoYun <ghdbs4959@naver.com>
2019-04-11 19:29:14 +0900

    bookinfo layout

--- app/src/main/java/com/example/gonami/bookboxbook/AddBook/BookSettingActivity.java
+++ app/src/main/java/com/example/gonami/bookboxbook/AddBook/BookSettingActivity.java
@@ -3,10 +3,37 @@ package com.example.gonami.bookboxbook.AddBook;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.example.gonami.bookboxbook.R;
 
 public class BookSettingActivity extends AppCompatActivity {
+    private TextView tv_sell;
+    private TextView tv_writing;
+    private CheckBox check_underline1;
+    private CheckBox check_underline2;
+    private CheckBox check_writing1;
+    private CheckBox check_writing2;
+
+    private TextView tv_cover;
+    private CheckBox check_clean;
+    private CheckBox check_name;
+    private CheckBox check_damage1;
+    private CheckBox check_damage2;
+
+    private TextView tv_photo;
+    private ImageButton image1;
+    private ImageButton image2;
+
+    private TextView tv_location;
+
+    private TextView tv_memo;
+    private
+
+    private Button btn_regist;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
